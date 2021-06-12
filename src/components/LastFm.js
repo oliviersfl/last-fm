d425938464d09d4d1a6d107f410892d65cc3ab9a
Olivier <oliviersfl@gmail.com>
2021-06-12 21:27:28 +0400

    Display Search results in lastFm

--- src/components/LastFm.js
+++ src/components/LastFm.js
@@ -1,20 +1,27 @@
+import React, { useState } from 'react';
 import LastFmApi from '../api/lastfm';
 import LastFmSearch from './Search/LastFmSearch';
-
-function searchArtist(term) {
-  LastFmApi.get("", {
-    params: {
-      artist: term
-    }
-  }).then((response) => {
-    
-  });
-}
+import LastFmSearchResult from './LastFmSearchResult';
 
 const LastFm = ({ visible }) => {
+  const [searchResults, setSearchResults] = useState(null);
+
+  const searchArtist = (term) => {
+    LastFmApi.get("", {
+      params: {
+        artist: term
+      }
+    }).then((response) => {
+      setSearchResults(response.data);
+    }, (error) => {
+      
+    });
+  }
+
   return (
     <div className={ "ui segment content" + (visible ? "" : " hidden") }>
       <LastFmSearch onFormSubmit={ searchArtist } />
+      <LastFmSearchResult artists={ searchResults == null ? null : searchResults.results.artistmatches.artist }/>
     </div>
   );
 }
---
+++ src/components/LastFmSearchResult.js
@@ -0,0 +1,34 @@
+const LastFmSearchResult = ( {artists }) => {
+    return (
+        <div className="ui segment content">
+            <table className="ui selectable celled table">
+                <thead>
+                    <tr>
+                        <th className="two wide"></th>
+                        <th className="twelve wide">Artist Name</th>
+                        <th className="two wide"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        artists == null ? "" : artists.map((artist) => {
+                            return (
+                                <tr>
+                                    <td>
+                                        <div className="ui image">
+                                            <img src={ artist.image[1]["#text"] } />
+                                        </div>
+                                    </td>
+                                    <td>{ artist.name }</td>
+                                    <td></td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default LastFmSearchResult;
