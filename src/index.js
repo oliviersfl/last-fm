edb1c3bc6afe1a2dc8603331b4eaeae23150e887
Olivier <oliviersfl@gmail.com>
2021-06-13 08:04:04 +0400

    Import semantic js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-css/semantic.min.js';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
