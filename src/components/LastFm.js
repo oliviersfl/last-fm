89d9d4795057b28d1bf4a30e0c07866b5b4784b0
Olivier <oliviersfl@gmail.com>
2021-06-13 16:12:51 +0400

    Add semantic-ui-react and Modal for Shortlist

--- src/components/LastFm.js
+++ src/components/LastFm.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import LastFmApi from '../api/lastfm';
 import LastFmSearch from './Search/LastFmSearch';
 import LastFmSearchResult from './SearchResult/LastFmSearchResult';
-import $ from 'jquery';
+import { Button, Modal } from 'semantic-ui-react'
 
 const LastFm = ({ visible }) => {
   const [searchResults, setSearchResults] = useState(null);
+  const [open, setOpen] = React.useState(false)
 
   const searchArtist = (term) => {
     LastFmApi.get("", {
@@ -25,12 +26,39 @@ const LastFm = ({ visible }) => {
         <tbody>
           <tr>
             <td className="thirteen wide"></td>
-            <td className="three wide right aligned"><button className="ui button center aligned green">Show short-list</button></td>
+            <td className="three wide right aligned">
+            <Modal
+              closeIcon
+              onClose={ () => setOpen(false) }
+              onOpen={ () => setOpen(true) }
+              open={ open }
+              trigger={<Button className="ui button center aligned green">Show short-list</Button>}
+            >
+              <Modal.Header>Your Shortlist</Modal.Header>
+              <Modal.Content>
+                <table className="ui very basic table">
+                  <thead>
+                    <tr>
+                      <th className="one wide"></th>
+                      <th className="fifteen wide">Artist Name</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><i className="star outline icon orange pointer"></i></td>
+                      <td className="left aligned">Artist name</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Modal.Content>
+            </Modal>
+            </td>
           </tr>
         </tbody>
       </table>
       <LastFmSearch onFormSubmit={ searchArtist } />
       <LastFmSearchResult artists={ (searchResults != null && searchResults.results) && searchResults.results.artistmatches.artist } />
+      
     </div>
   );
 }
