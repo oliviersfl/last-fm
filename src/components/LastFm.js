1295a72a74b9aae03ec51af218124e8d700c7581
Olivier <oliviersfl@gmail.com>
2021-06-13 15:32:24 +0400

    Add Button to show shortlist

--- src/components/LastFm.js
+++ src/components/LastFm.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import LastFmApi from '../api/lastfm';
 import LastFmSearch from './Search/LastFmSearch';
 import LastFmSearchResult from './SearchResult/LastFmSearchResult';
+import $ from 'jquery';
 
 const LastFm = ({ visible }) => {
   const [searchResults, setSearchResults] = useState(null);
@@ -20,8 +21,16 @@ const LastFm = ({ visible }) => {
 
   return (
     <div className={ "ui segment content" + (!visible ? " hidden" : "") }>
+      <table className="ui very basic table">
+        <tbody>
+          <tr>
+            <td className="thirteen wide"></td>
+            <td className="three wide right aligned"><button className="ui button center aligned green">Show short-list</button></td>
+          </tr>
+        </tbody>
+      </table>
       <LastFmSearch onFormSubmit={ searchArtist } />
-      <LastFmSearchResult artists={ (searchResults != null && searchResults.results) && searchResults.results.artistmatches.artist }/>
+      <LastFmSearchResult artists={ (searchResults != null && searchResults.results) && searchResults.results.artistmatches.artist } />
     </div>
   );
 }
