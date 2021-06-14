fae0270ff0db9ce73c8262fa7379eb814d25268a
Olivier <oliviersfl@gmail.com>
2021-06-14 20:39:44 +0400

    Add MusicBrainz favourite releases/artists

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
 import LastFmFavourite from "./Favourites/LastFmFavourite";
 import MusicBrainzFavourite from "./Favourites/MusicBrainzFavourite";
 import { Table } from 'semantic-ui-react'
 
 const Favourites = ({ visible, favourites, setFavourites }) => {
+    const [activeArtistRelease, setActiveArtistRelease] = useState(null);
     return (
         <div className={ "ui segment content" + (!visible && " hidden") }>
             <h1>Favourites</h1>
@@ -20,9 +22,16 @@ const Favourites = ({ visible, favourites, setFavourites }) => {
                 {
                     favourites.map(favourite => {
                         return typeof(favourite) === "string" ?
-                        <LastFmFavourite favouriteArtist={ favourite } favourites={ favourites } setFavourites={ setFavourites }/>
+                        <LastFmFavourite key={ favourite.id } favouriteArtist={ favourite } favourites={ favourites } setFavourites={ setFavourites }/>
                         :
-                        <MusicBrainzFavourite favouriteRelease={ favourite } favourites={ favourites } setFavourites={ setFavourites } />
+                        <MusicBrainzFavourite
+                            key={ favourite.id }
+                            favouriteRelease={ favourite }
+                            favourites={ favourites }
+                            setFavourites={ setFavourites }
+                            activeArtistRelease={ activeArtistRelease }
+                            setActiveArtistRelease={ setActiveArtistRelease }
+                        />
                     })
                 }
                 </Table.Body>
--- src/components/Favourites/MusicBrainzFavourite.js
+++ src/components/Favourites/MusicBrainzFavourite.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import { Icon, Button, Table } from 'semantic-ui-react';
 import { useCookies } from 'react-cookie';
 
-const MusicBrainzFavourite = ({ favouriteRelease, favourites, setFavourites }) => {
+const MusicBrainzFavourite = ({ favouriteRelease, favourites, setFavourites, activeArtistRelease, setActiveArtistRelease }) => {
     const [cookies, setCookie] = useCookies(['favArtists']);
 
     const removeArtist = () => {
@@ -12,10 +13,8 @@ const MusicBrainzFavourite = ({ favouriteRelease, favourites, setFavourites }) =
             }
 
             if(favourites[i].id === favouriteRelease.id) {
-                console.warn("release to be removed found");
                 fav.splice(i, 1);
                 setFavourites(fav);
-                console.log(fav);
                 var date = new Date(Date.now());
                 setCookie('favArtists', JSON.stringify(fav), { expires: new Date(date.getFullYear() + 1, date.getMonth(), date.getDate()) });
                 return;
@@ -23,12 +22,67 @@ const MusicBrainzFavourite = ({ favouriteRelease, favourites, setFavourites }) =
         }
     }
 
+    const removeRelease = (releaseId) => {
+        var fav = favourites.slice();
+
+        for(var i = 0; i < fav.length; i++) {
+            if(typeof(fav[i]) == "string") {
+                continue;
+            }
+            for(var j = 0; j < fav[i].releases.length; j++) {
+                if(fav[i].releases[j].id === releaseId) {
+                    fav[i].releases.splice(j, 1);
+
+                    if(fav[i].releases.length === 0) {
+                        fav.splice(i, 1);
+                    }
+                    setFavourites(fav);
+                    var date = new Date(Date.now());
+                    setCookie('favArtists', JSON.stringify(fav), { expires: new Date(date.getFullYear() + 1, date.getMonth(), date.getDate()) });
+                    return;
+                }
+            }
+        }
+    }
+
     return (
-        <Table.Row>
-            <Table.Cell textAlign='center'><Icon color='red' name='minus circle' className='pointer' size='large' onClick={ () => removeArtist() } /></Table.Cell>
-            <Table.Cell>{ favouriteRelease.name }</Table.Cell>
-            <Table.Cell textAlign='center'><Button basic color='green' content='Show Releases' /></Table.Cell>
-        </Table.Row>
+        <React.Fragment>
+            <Table.Row>
+                <Table.Cell textAlign='center'><Icon color='red' name='minus circle' className='pointer' size='large' onClick={ () => removeArtist() } /></Table.Cell>
+                <Table.Cell>{ favouriteRelease.name }</Table.Cell>
+                <Table.Cell textAlign='center'><Button basic color='green' content={ (favouriteRelease.id === activeArtistRelease ? 'Hide' : 'Show') + ' Releases' } onClick={ () => setActiveArtistRelease(favouriteRelease.id === activeArtistRelease ? null : favouriteRelease.id) } /></Table.Cell>
+            </Table.Row>
+            <Table.Row className={ favouriteRelease.id === activeArtistRelease ? "" : "hidden"}>
+                <Table.Cell colSpan="3">
+                    <Table>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell></Table.HeaderCell>
+                                <Table.HeaderCell>Year</Table.HeaderCell>
+                                <Table.HeaderCell>Title</Table.HeaderCell>
+                                <Table.HeaderCell>Release Label</Table.HeaderCell>
+                                <Table.HeaderCell>Number of tracks</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                        {
+                            favouriteRelease.releases.map(release => {
+                                return (
+                                    <Table.Row key={ release.id }>
+                                        <Table.Cell><Icon color='red' name='minus circle' className='pointer' size='large' onClick={ () => removeRelease(release.id) } /></Table.Cell>
+                                        <Table.Cell>{ release.year }</Table.Cell>
+                                        <Table.Cell>{ release.title }</Table.Cell>
+                                        <Table.Cell>{ release.label }</Table.Cell>
+                                        <Table.Cell>{ release.noTracks }</Table.Cell>
+                                    </Table.Row>
+                                )
+                            })
+                        }
+                        </Table.Body>
+                    </Table>
+                </Table.Cell>
+            </Table.Row>
+        </React.Fragment>
     );
 }
 
