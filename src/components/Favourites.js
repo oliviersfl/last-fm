4e29b5f218b70f35c611f4ecd4607b05c79cd990
Olivier Sin <olivier.sin@castillelabs.com>
2021-06-14 16:20:42 +0400

    Display data from favourites

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,8 +1,23 @@
 function Favourites({ visible, favourites, setFavourites }) {
+    console.log(favourites);
     return (
         <div className={ "ui segment content" + (!visible && " hidden") }>
             <h1>Favourites</h1>
             <p>Count: { favourites.length }</p>
+
+            {
+                favourites.map(favourite => {
+                    return typeof(favourite) === "string" ?
+                    <p>{ favourite }</p>
+                    :
+                    <div>
+                        <h2>{ favourite.name }</h2>
+                        { favourite.releases.map(release => {
+                            return <p><i>{ release.title }</i></p>
+                        }) }
+                    </div>
+                })
+            }
         </div>
     )
 }
