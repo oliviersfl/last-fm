b27f6de3c75f8c60b0333f6d2eb68eac3325f3d5
Olivier <oliviersfl@gmail.com>
2021-06-14 21:32:44 +0400

    Add empty favourite message

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import LastFmFavourite from "./Favourites/LastFmFavourite";
 import MusicBrainzFavourite from "./Favourites/MusicBrainzFavourite";
 import { Table } from 'semantic-ui-react'
+import { Message } from 'semantic-ui-react'
 
 const Favourites = ({ visible, favourites, setFavourites }) => {
     const [activeArtistRelease, setActiveArtistRelease] = useState(null);
     return (
         <div className={ "ui segment content" + (!visible && " hidden") }>
             <h1>Favourites</h1>
-            <p>Count: { favourites.length }</p>
 
-            <Table celled>
+            <Message className={ favourites.length === 0 ? "" : "hidden" }>
+                <Message.Header>No favourites added</Message.Header>
+                <p>You haven't added any favourites yet. Please search for your favourite artist and releases to add to this list.</p>
+            </Message>
+
+            <Table celled className={ favourites.length === 0 ? "hidden" : "" }>
                 <Table.Header>
                     <Table.Row>
                         <Table.HeaderCell width='1'></Table.HeaderCell>
